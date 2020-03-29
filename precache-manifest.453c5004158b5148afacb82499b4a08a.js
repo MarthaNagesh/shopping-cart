521602b7c16658761c6375eed2972506a22f807b
nagesh <marthanagesh@gmail.com>
2020-03-29 13:58:27 -0400

    Updates

---
+++ precache-manifest.453c5004158b5148afacb82499b4a08a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1291dc431f976635e912e6734e9bfc4e",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "67b8c3958273e560d6aa",
+    "url": "/shopping-cart/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8a9155169ca64306f5bd",
+    "url": "/shopping-cart/static/css/main.59800085.chunk.css"
+  },
+  {
+    "revision": "67b8c3958273e560d6aa",
+    "url": "/shopping-cart/static/js/2.7ca9cbdf.chunk.js"
+  },
+  {
+    "revision": "8a9155169ca64306f5bd",
+    "url": "/shopping-cart/static/js/main.31c42979.chunk.js"
+  },
+  {
+    "revision": "81ed0492ebbe54d87b8a",
+    "url": "/shopping-cart/static/js/runtime-main.2ac8cde8.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/shopping-cart/static/media/logo.9f7e506d.svg"
+  }
+]);
